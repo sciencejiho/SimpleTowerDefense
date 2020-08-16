9a86947af1fa9134ec599449a4c10f0ef1c120ae
JiHo Han <sciencejiho@gmail.com>
2020-08-16 20:21:41 +0900

    build(map): add LevelManager for map generation

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Responsible for creating levels for the game
+public class LevelManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject tile;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CreateLevel();
+    }
+
+    // Update is called once per frame
+    void Update() {}
+
+    //
+    private void CreateLevel()
+    {
+        float tileSize = tile.GetComponent<SpriteRenderer>().sprite.bounds.size.x;
+
+        for (int j = 0; j < 5; j++)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                GameObject newTile = Instantiate(tile);
+                newTile.transform.position = new Vector3(tileSize * i, tileSize * j, 0);
+            }
+        }
+    }
+}
