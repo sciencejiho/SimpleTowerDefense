c56a12edda8cd99020423bd51fe12a3c5b56a530
JiHo Han <sciencejiho@gmail.com>
2020-08-16 21:53:20 +0900

    build(map): modify LevelManager so that map is filled with GreenGrass

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -3,32 +3,44 @@ using System.Collections.Generic;
 using UnityEngine;
 
 // Responsible for creating levels for the game
-public class LevelManager : MonoBehaviour
+public class LevelManager : MonoBehaviour 
 {
     [SerializeField]
     private GameObject tile;
 
-    // Start is called before the first frame update
-    void Start()
+    public float TileSize 
     {
-        CreateLevel();
+        get 
+        {
+            return tile.GetComponent<SpriteRenderer> ().sprite.bounds.size.x;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start () 
+    {
+        CreateLevel ();
     }
 
     // Update is called once per frame
-    void Update() {}
-
-    //
-    private void CreateLevel()
+    void Update () { }
+    
+    private void CreateLevel () 
     {
-        float tileSize = tile.GetComponent<SpriteRenderer>().sprite.bounds.size.x;
-
-        for (int j = 0; j < 5; j++)
+        // Get the top left corner of the map
+        Vector3 worldStart = Camera.main.ScreenToWorldPoint(new Vector3(0, Screen.height));
+        for (int j = 0; j < 8; j++) 
         {
-            for (int i = 0; i < 5; i++)
+            for (int i = 0; i < 13; i++) 
             {
-                GameObject newTile = Instantiate(tile);
-                newTile.transform.position = new Vector3(tileSize * i, tileSize * j, 0);
+                PlaceTile (i, j, worldStart);
             }
         }
+    }
+
+    private void PlaceTile (int x, int y, Vector3 worldStart) 
+    {
+        GameObject newTile = Instantiate (tile);
+        newTile.transform.position = new Vector3 (worldStart.x + (TileSize * x), worldStart.y - (TileSize * y), 0);
     }
 }
