db474b6c0d11fc4ce81adbd4e5173fa827454841
JiHo Han <sciencejiho@gmail.com>
2020-08-19 00:06:26 +0900

    build(camer):add CameraMovement to move the camera on maps only

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour 
+{
+    [SerializeField]
+    private float cameraSpeed = 0;
+    private float xMax, yMin;
+
+    // Start is called before the first frame update
+    void Start () { }
+
+    // Update is called once per frame
+    void Update () 
+    {
+        GetInput();
+    }
+
+    private void GetInput()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            transform.Translate(Vector3.up * cameraSpeed * Time.deltaTime);
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            transform.Translate(Vector3.left * cameraSpeed * Time.deltaTime);
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            transform.Translate(Vector3.down * cameraSpeed * Time.deltaTime);
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            transform.Translate(Vector3.right * cameraSpeed * Time.deltaTime);
+        }
+
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x, 0, xMax), Mathf.Clamp(transform.position.y, yMin, 0), -10);
+    }
+
+    public void SetLimits(Vector3 maxTile)
+    {
+        Vector3 p = Camera.main.ViewportToWorldPoint(new Vector3(1, 0));
+
+        xMax = maxTile.x - p.x;
+        yMin = maxTile.y - p.y;
+    }
+}
--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -8,6 +8,9 @@ public class LevelManager : MonoBehaviour {
     [SerializeField]
     private GameObject[] tilePrefabs;
 
+    [SerializeField]
+    private CameraMovement cameraMovement;
+
     public float TileSize {
         get {
             return tilePrefabs[0].GetComponent<SpriteRenderer> ().sprite.bounds.size.x;
@@ -30,28 +33,41 @@ public class LevelManager : MonoBehaviour {
         // Default: 8
         int mapYSize = mapData.Length;
 
+        Vector3 maxTile = Vector3.zero;
+
         Debug.Log(mapXSize);
         Debug.Log(mapYSize);
 
         // Get the top left corner of the map
         Vector3 worldStart = Camera.main.ScreenToWorldPoint (new Vector3 (0, Screen.height));
+        
+        // Y position of the map
         for (int j = 0; j < mapYSize; j++) 
         {
+            // Gets all the tiles that we need to place
             char[] newTiles = mapData[j].ToCharArray ();
 
+            // X position of the map
             for (int i = 0; i < mapXSize; i++) 
             {
-                PlaceTile (newTiles[i].ToString (), i, j, worldStart);
+                maxTile = PlaceTile (newTiles[i].ToString(), i, j, worldStart);
             }
         }
+
+        cameraMovement.SetLimits(new Vector3(maxTile.x + TileSize, maxTile.y - TileSize));
     }
 
-    private void PlaceTile (string tileType, int x, int y, Vector3 worldStart) {
+    private Vector3 PlaceTile (string tileType, int x, int y, Vector3 worldStart) {
         // Parse string value into int type
         int tileIndex = int.Parse (tileType);
 
+        // Creates a new tile and makes a reference to that tile in the newTile variable
         GameObject newTile = Instantiate (tilePrefabs[tileIndex]);
+
+        // Uses the newTile variable to change the position of the tile
         newTile.transform.position = new Vector3 (worldStart.x + (TileSize * x), worldStart.y - (TileSize * y), 0);
+    
+        return newTile.transform.position;
     }
 
     private string[] ReadLevelText () 
